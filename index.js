a6a0e2eb3f55d0cfde2646c33064d5be16804064
Noda Shimpei <noda.sin@gmail.com>
2016-10-22 20:23:57 +0900

    Fixed bug

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var AWS    = require('aws-sdk')
   , async  = require('async')
   , domain = require('domain');
 
+Array.prototype.flatten = function() {
+  return Array.prototype.concat.apply([], this);
+};
+
 AWS.config.update({region:'ap-northeast-1'});
 
 var DryRun = process.env.DRY_RUN || false;
@@ -60,8 +64,11 @@ exports.handler = function(event, context, callback) {
   }, d.intercept(function (data) {
 
     var instances = data
-    .Reservations[0]
-    .Instances
+    .Reservations
+    .map(function (reservation) {
+      return reservation.Instances
+    })
+    .flatten()
     .map(function (instance) {
       var powerOnTagValue  = getInstanceTag(instance, 'PowerOn');
       var powerOffTagValue = getInstanceTag(instance, 'PowerOff');
@@ -82,20 +89,30 @@ exports.handler = function(event, context, callback) {
 
     var now = getJSTNowDate();
 
+    console.log('Instances Having Tag: [' + instances.map(function (instance) {
+        return instance.id;
+    }) + ']');
+
     var powerOnTargets = instances.filter(function (instance) {
       if (instance.state !== 'stopped' || null === instance.powerOnDate) {
         return false;
       }
 
-      console.log('instance power on date: ' + instance.powerOnDate);
-      console.log('now date: ' + now);
+      console.log('[' + instance.id + '] Instance is stopped');
+      console.log('[' + instance.id + '] Instance power on date: ' + instance.powerOnDate);
+      console.log('Now date is ' + now);
+
+      var willBeLaunched = instance.powerOffDate.getTime() < now.getTime();
 
       if (null === instance.powerOffDate) {
-        return instance.powerOnDate.getTime() < now.getTime();
+        willBeLaunched = instance.powerOnDate.getTime() < now.getTime();
+      } else {
+        willBeLaunched = (instance.powerOnDate.getTime() < now.getTime()) && (instance.powerOffDate.getTime() > now.getTime());
       }
 
-      return (instance.powerOnDate.getTime() < now.getTime()) &&
-             (instance.powerOffDate.getTime() > now.getTime());
+      console.log('[' + instance.id + '] Instance will be launched: ' + willBeLaunched);
+
+      return willBeLaunched;
     }).map(function (instance) {
       return instance.id;
     });
@@ -105,16 +122,21 @@ exports.handler = function(event, context, callback) {
         return false;
       }
 
-      console.log('instance power off date: ' + instance.powerOffDate);
-      console.log('now date: ' + now);
-      
-      return instance.powerOffDate.getTime() < now.getTime();
+      console.log('[' + instance.id + '] Instance is running');
+      console.log('[' + instance.id + '] Instance power off date: ' + instance.powerOnDate);
+      console.log('[' + instance.id + '] Now date is ' + now);
+
+      var willBeStopped = instance.powerOffDate.getTime() < now.getTime();
+
+      console.log('[' + instance.id + '] Instance will be stopped: ' + willBeStopped);
+
+      return willBeStopped;
     }).map(function (instance) {
       return instance.id;
     });
 
-    console.log('Power on  instnaces: [' + powerOnTargets  + ']');
-    console.log('Power off instnaces: [' + powerOffTargets + ']');
+    console.log('Power on  taget\'s instnaces: [' + powerOnTargets  + ']');
+    console.log('Power off taget\'s instnaces: [' + powerOffTargets + ']');
 
     async.parallel([
       function(callback) {
